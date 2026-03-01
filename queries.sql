7ec65a5519932e32b1b563d9dbe1b8aa647e4c3c
sulekhabidhan88 <sulekhabidhan768@gmail.com>
2026-03-02 02:30:32 +0530

    ADDED SQL BOLT answers

---
+++ queries.sql
@@ -0,0 +1,11 @@
+--
+--Q1 Find the title of each film
+SELECT title FROM movies;
+--Q2 Find the director of each film ✓
+SELECT director FROM movies;
+--Q3 Find the title and director of each film 
+SELECT title, director FROM movies;
+--Q4 Find the title and year of each film
+SELECT title, year FROM movies;
+--Q5 Find all the information about each film 
+SELECT * FROM movies;
